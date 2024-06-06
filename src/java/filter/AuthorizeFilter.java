00ce0f70fd9ad662b7938c2bc089cee436bf5e7e
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-06-06 22:26:44 +0700

    feat: init filter

---
+++ src/java/filter/AuthorizeFilter.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
+ */
+package filter;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author hoang hung
+ */
+public class AuthorizeFilter implements Filter {
+
+    /**
+     *
+     * @param servletRequest
+     * @param servletResponse
+     * @param filterChain
+     * @throws IOException
+     * @throws ServletException
+     */
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        // Implement authorize here
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
