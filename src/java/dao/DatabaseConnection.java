3b98c8fe03065f1afe15044b6f44d5c1bfb79fd9
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-06-06 22:23:29 +0700

    ref: move DatabaseConnection.java to package dao

---
+++ src/java/dao/DatabaseConnection.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDriver;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author hoang hung
+ */
+public class DatabaseConnection {
+    private static Connection connection;
+    private static final String URL = "jdbc:sqlserver://localhost:1433;database=FinalProjectPRJ301;encrypt=true;trustServerCertificate=true;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
+    private static final String USERNAME = "";
+    private static final String PASSWORD = "";
+    
+    public DatabaseConnection() {
+    }
+    
+    public static void createConnection() {
+        try {
+            DriverManager.registerDriver(new SQLServerDriver());
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+
+    public static Connection getConnection() {
+        try {
+            if (connection == null || connection.isClosed()) {
+                createConnection();
+            }    
+        } catch (SQLException e) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, e);
+        }
+        
+        return connection;
+    }
+    
+    public static void setConnection() {
+        connection = getConnection();
+                
+    }
+    
+    public static void closeConnection() {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+	
+    public static void printInfo() {
+        try {
+            if (connection != null) {
+                DatabaseMetaData mtdt = connection.getMetaData();
+                System.out.println(mtdt.getDatabaseProductName());
+                System.out.println(mtdt.getDatabaseProductVersion());
+            }
+        } catch (SQLException e) {
+            Logger.getLogger(DatabaseConnection.class.getName()).log(Level.SEVERE, null, e);
+        }
+    }
+}
