e91a57b3cc968c70e9585436941eda78ad870a3a
Hoang Vu Hung <hoanghung250604@gmail.com>
2024-06-22 17:30:25 +0700

    Implement model User

---
+++ src/java/model/User.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import enums.Gender;
+import enums.Role;
+import java.util.Date;
+
+/**
+ *
+ * @author hoang hung
+ */
+public class User {
+    
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String phone;
+    private Gender gender;
+    private Date dob;
+    private Date joinAt;
+    private String avatarUrl;
+    private String address;
+    private Role role;
+    private int balance;
+
+    public User() {
+    }
+
+    public User(int id, String username, String password, String email, String phone, Gender gender, Date dob, Date joinAt, String avatarUrl, String address, Role role, int balance) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.phone = phone;
+        this.gender = gender;
+        this.dob = dob;
+        this.joinAt = joinAt;
+        this.avatarUrl = avatarUrl;
+        this.address = address;
+        this.role = role;
+        this.balance = balance;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username=" + username + ", password=" + password + ", email=" + email + ", phone=" + phone + ", gender=" + gender + ", dob=" + dob + ", joinAt=" + joinAt + ", avatarUrl=" + avatarUrl + ", address=" + address + ", role=" + role + ", balance=" + balance + '}';
+    }
+}
